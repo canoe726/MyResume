b1fc12521372fbfdc540505a72c1715ac6aab21c
canoe726 <canoe918@gmail.com>
2020-07-20 03:44:23 +0900

    add exp-chart code

--- public/js/myprofile.js
+++ public/js/myprofile.js
@@ -1,3 +1,7 @@
+String.prototype.replaceAll = function(org, dest) {
+  return this.split(org).join(dest);
+}
+
 window.onscroll = function() {
   scrollUpFunc();
 };
@@ -34,6 +38,137 @@ for(i=0; i<coll.length; i++) {
       var padding = this.parentNode.parentNode.previousElementSibling;
       padding.childNodes[0].style.marginBottom = "0px";
     }
+  });
+}
+
+var show_chart = document.querySelector('.experience-graph');
+if(show_chart.innerHTML !== '') {
+  var value = document.querySelector('#chart-value');
+  var input_value = value.value;
+  input_value = input_value.replaceAll('\'','\"');
+  var chart_value = JSON.parse(input_value);
+  
+  var labels = [];
+  var dates = [];
+  var icons = [];
+
+  if(chart_value.high_school !== undefined) {
+    labels.push('고등학교');
+    dates.push([new Date(chart_value.high_school[0]), new Date(chart_value.high_school[1])]);
+
+  } 
+  if(chart_value.university !== undefined) {
+    labels.push('대학교');
+    dates.push([new Date(chart_value.university[0]), new Date(chart_value.university[1])]);
+
+  } 
+  if(chart_value.job_exp !== undefined) {
+    for(var i=0; i<chart_value.job_exp.length; i++) {
+      labels.push('직무경험');
+      dates.push([new Date(chart_value.job_exp[i][0]), new Date(chart_value.job_exp[i][1])]);
+    }
+  } 
+  if(chart_value.global_exp !== undefined) {
+    for(var i=0; i<chart_value.global_exp.length; i++) {
+      labels.push('해외경험');
+      dates.push([new Date(chart_value.global_exp[i][0]), new Date(chart_value.global_exp[i][1])]);
+    }
+  } 
+  if(chart_value.other_exp !== undefined) {
+    for(var i=0; i<chart_value.other_exp.length; i++) {
+      labels.push('기타경험');
+      dates.push([new Date(chart_value.other_exp[i][0]), new Date(chart_value.other_exp[i][1])]);
+    }
+  }
+
+  for(var i=0; i<labels.length; i++) {
+      if (labels[i] === '고등학교') { icons[i] = '\uf19c' }
+      else if (labels[i] === '대학교') { icons[i] = '\uf19d' }
+      else if (labels[i] === '직무경험') { icons[i] = '\uf0c0' }
+      else if (labels[i] === '해외경험') { icons[i] = '\uf0ac' }
+      else if (labels[i] === '기타경험') { icons[i] = '\uf234' }
+  }
+
+  var backgroundColors = [];
+
+  for(var i=0; i<50; i++) {
+      var r = Math.floor(Math.random() * 255);
+      var g = Math.floor(Math.random() * 255);
+      var b = Math.floor(Math.random() * 255);
+      backgroundColors[i] = `rgba(${r}, ${g}, ${b}, 0.5)`;
+  }
+
+  moment.lang('kr');
+
+  var min_datetime = dates[0][0];
+  for(var i=0; i<dates.length; i++) {
+    if(min_datetime > dates[i][0]) { min_datetime = dates[i][0]; }
+  }
+
+  var max_datetime = dates[0][1];
+  for(var i=0; i<dates.length; i++) {
+    if(max_datetime < dates[i][1]) { max_datetime = dates[i][1]; }
+  }
+  max_datetime = moment(max_datetime).add(1, 'year');
+
+  var ctx = document.getElementById('exp-chart').getContext("2d");
+  var myChart = new Chart(ctx, {
+      type: 'horizontalBar',
+      legend: {
+          display: false
+      },
+      data: {
+          labels: labels,
+          datasets: [{
+              label: '경험 그래프',
+              data: dates,
+              icons: icons,
+              backgroundColor: backgroundColors,
+          }]
+      },
+      options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              xAxes: [{
+                  gridLines: {
+                      display: false
+                  },
+                  type: 'time',
+                  time: {
+                      unit: 'year',
+                      displayFormats: {
+                          day : 'YYYY-MM'
+                      }
+                  },
+                  ticks: {
+                      min: min_datetime,
+                      max: max_datetime,
+                  }
+              }],
+              yAxes: [{
+                  ticks: {
+                      reverse: true,
+                      beginAtZero: true,
+                  }
+              }]
+          },
+          plugins: {
+            datalabels: {
+              align: 'end',
+              anchor: 'end',
+              color: "#cc55aa",
+              font: {
+                family: 'FontAwesome',
+                size: 20
+              },
+              formatter: function(value, context) {
+                return context.dataset.icons[context.dataIndex];
+              }
+            }
+          }
+      }
   });
 }
 
